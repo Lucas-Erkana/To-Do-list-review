81a3cda3c675c7e9186cf0bc82fd654ec3219f1f
Lucas David Erkana <lderkana21x@gmail.com>
2022-12-13 14:52:38 +0200

    Add date.js and luxion.js

--- src/modules/variables.js
+++ src/modules/variables.js
@@ -2,5 +2,3 @@ export const form = document.getElementById('todoform');
 export const todoInput = document.getElementById('newtodo');
 export const todosListEl = document.getElementById('todos-list');
 export const notificationEl = document.querySelector('.notification');
-export const todos = JSON.parse(localStorage.getItem('todos')) || [];
-export const EditTodoId = -1;
